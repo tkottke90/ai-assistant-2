c0864f83c9c3e70110de7d9e41d3103d293e97c8
Thomas Kottke <t.kottke90@gmail.com>
2026-02-21 12:36:01 -0600

    feat: add shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact(), tailwindcss()],
-
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      'lucide-react': 'lucide-preact',
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+      'react/jsx-runtime': 'preact/jsx-runtime' // Important for JSX transformations
+    },
+    
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:6060',

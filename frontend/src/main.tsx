326fbda692eb0bd9f3e68bbb22487177f9c4d848
Thomas Kottke <t.kottke90@gmail.com>
2026-02-26 16:05:58 -0600

    refactor: style chat messages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { render } from 'preact'
 import './index.css'
 import App from './app.tsx'
 import ChatHistory from './routes/chat/chat-history';
+import 'highlight.js';
 
 ChatHistory.initialize();
 
